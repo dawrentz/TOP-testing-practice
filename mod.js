dbfd7f9956f905316b331e1410e3f5ee08908686
dawrentz <156712569+dawrentz@users.noreply.github.com>
2025-01-17 16:16:34 -0600

    Set up environment
    Make sure node and jest run. Also make sure project runs on browser.

---
+++ mod.js
@@ -0,0 +1,5 @@
+console.log("from mod");
+
+export function testFunc() {
+  return 777;
+}
---
+++ mod.test.js
@@ -0,0 +1,5 @@
+import * as mod from "./mod.js";
+
+test("works", () => {
+  expect(mod.testFunc()).toBe(777);
+});
